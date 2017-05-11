3d2a2e1fec358cc3c07b9cd47f27ad8f361bc691
Nathan Chancellor <natechancellor@gmail.com>
2017-05-10 22:22:50 -0700

    lazyplug: Only use super aggressive profile when screen is off
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    
     Conflicts:
    	drivers/media/platform/msm/vidc/msm_vidc.c
    	drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -24,7 +24,7 @@
 
 #define MAX_EVENTS 30
 
-extern void lazyplug_enter_lazy(bool enter);
+extern void lazyplug_enter_lazy(bool enter, bool video);
 
 static int get_poll_flags(void *instance)
 {
@@ -1348,6 +1348,7 @@ void *msm_vidc_open(int core_id, int session_type)
 			"Failed to move video instance to init state\n");
 		goto fail_init;
 	}
+
 	inst->debugfs_root =
 		msm_vidc_debugfs_init_inst(inst, core->debugfs_root);
 
@@ -1358,7 +1359,7 @@ void *msm_vidc_open(int core_id, int session_type)
 		goto fail_setup;
 	}
 
-	lazyplug_enter_lazy(true);
+	lazyplug_enter_lazy(true, true);
 
 	return inst;
 
@@ -1490,7 +1491,7 @@ int msm_vidc_close(void *instance)
 			VIDC_MSG_PRIO2STRING(VIDC_INFO), inst);
 	kfree(inst);
 
-	lazyplug_enter_lazy(false);
+	lazyplug_enter_lazy(false, true);
 
 	return 0;
 }
--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -95,6 +95,8 @@ bool is_display_on()
 	return display_on;
 }
 
+extern void lazyplug_enter_lazy(bool enter, bool video);
+
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	if (ctrl->pwm_pmi)
@@ -894,6 +896,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 
 	display_on = true;
+	lazyplug_enter_lazy(false, false);
 
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -1035,6 +1038,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	mdss_dsi_panel_off_in_prog_notify(pdata, pinfo);
 	display_on = false;
+	if ( (asus_lcd_id[0]=='2') || (asus_lcd_id[0]=='3') )
+	    resume2s=0;
+
+        lazyplug_enter_lazy(true, false);
 
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
