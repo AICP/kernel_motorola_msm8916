0164145d16a9326ff4581408f1dced2bde8641b8
Pranav Vashi <neobuddy89@gmail.com>
2018-10-15 15:35:09 +0900

    drivers: cpufreq: Add impulse govorner
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -450,6 +450,9 @@ extern struct cpufreq_governor cpufreq_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_TRIPNDROID)
 extern struct cpufreq_governor cpufreq_gov_tripndroid;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_tripndroid)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
